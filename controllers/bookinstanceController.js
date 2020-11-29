6e8db804730ac3be388609f9716bdf07e5fd9473
Sandro Peirano <sandro.peirano@gdpteam.com>
2020-11-28 21:30:14 -0500

    Add delete

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,4 +1,8 @@
 var BookInstance = require("../models/bookinstance");
+var Book = require("../models/book");
+var async = require("async");
+
+const { body, validationResult } = require("express-validator");
 
 // Display list of all BookInstances.
 exports.bookinstance_list = function (req, res, next) {
@@ -16,41 +20,122 @@ exports.bookinstance_list = function (req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res, next) {
-
+exports.bookinstance_detail = function (req, res, next) {
   BookInstance.findById(req.params.id)
-  .populate('book')
-  .exec(function (err, bookinstance) {
-    if (err) { return next(err); }
-    if (bookinstance==null) { // No results.
-        var err = new Error('Book copy not found');
+    .populate("book")
+    .exec(function (err, bookinstance) {
+      if (err) {
+        return next(err);
+      }
+      if (bookinstance == null) {
+        // No results.
+        var err = new Error("Book copy not found");
         err.status = 404;
         return next(err);
       }
-    // Successful, so render.
-    res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
-  })
-
+      // Successful, so render.
+      res.render("bookinstance_detail", {
+        title: "Copy: " + bookinstance.book.title,
+        bookinstance: bookinstance,
+      });
+    });
 };
 
 // Display BookInstance create form on GET.
-exports.bookinstance_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance create GET");
+exports.bookinstance_create_get = function (req, res, next) {
+  Book.find({}, "title").exec(function (err, books) {
+    if (err) {
+      return next(err);
+    }
+    // Successful, so render.
+    res.render("bookinstance_form", {
+      title: "Create BookInstance",
+      book_list: books,
+    });
+  });
 };
 
 // Handle BookInstance create on POST.
-exports.bookinstance_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance create POST");
-};
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+  // Validate and sanitise fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    var bookinstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      Book.find({}, "title").exec(function (err, books) {
+        if (err) {
+          return next(err);
+        }
+        // Successful, so render.
+        res.render("bookinstance_form", {
+          title: "Create BookInstance",
+          book_list: books,
+          selected_book: bookinstance.book._id,
+          errors: errors.array(),
+          bookinstance: bookinstance,
+        });
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      bookinstance.save(function (err) {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new record.
+        res.redirect(bookinstance.url);
+      });
+    }
+  },
+];
 
 // Display BookInstance delete form on GET.
-exports.bookinstance_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+exports.bookinstance_delete_get = (req, res, next) => {
+  BookInstance.findById(req.params.id).exec((err, results) => {
+    if (err) return next(err);
+    if (results == null) {
+      res.redirect("/catalog/bookinstances");
+      return;
+    }
+    res.render("bookinstance_delete", {
+      title: "Delete Copy",
+      bookinstance: results,
+    });
+  });
 };
 
 // Handle BookInstance delete on POST.
-exports.bookinstance_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+exports.bookinstance_delete_post = (req, res, next) => {
+  BookInstance.findById(req.body.bookinstanceid).exec((err, result) => {
+    if (err) return next(err);
+    BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+      if (err) return next(err);
+      res.redirect("/catalog/bookinstances");
+    });
+  });
 };
 
 // Display BookInstance update form on GET.
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,6 +2,8 @@ var Genre = require("../models/genre");
 var Book = require("../models/book");
 var async = require("async");
 
+const { body, validationResult } = require("express-validator");
+
 // Display list of all Genre.
 exports.genre_list = function (req, res, next) {
   Genre.find()
@@ -45,24 +47,83 @@ exports.genre_detail = function (req, res, next) {
 
 // Display Genre create form on GET.
 exports.genre_create_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre create GET");
+  res.render("genre_form", { title: "Create Genre" });
 };
 
 // Handle Genre create on POST.
-exports.genre_create_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-};
+exports.genre_create_post = [
+  body("name", "Genre name required").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    var genre = new Genre({ name: req.body.name });
+    if (!errors.isEmpty()) {
+      res.render("genre_form", {
+        title: "Genre Form",
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Genre.findOne({ name: req.body.name }).exec(function (err, found_genre) {
+        if (err) return next(err);
+        if (found_genre) res.redirect(found_genre.url);
+        else {
+          genre.save(function (err) {
+            if (err) return next(err);
+            res.redirect(genre.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 // Display Genre delete form on GET.
-exports.genre_delete_get = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+exports.genre_delete_get = (req, res, next) => {
+  async.parallel(
+    {
+      genre: (cb) => Genre.findById(req.params.id).exec(cb),
+      genre_books: (cb) => Book.find({ genre: req.params.id }).exec(cb),
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (results.genre == null) res.redirect("/catalog/genres");
+      res.render("genre_delete", {
+        title: "Delete Genre",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
+exports.genre_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      genre: (cb) => Genre.findById(req.body.genreid).exec(cb),
+      genre_books: (cb) => Book.find({ genre: req.body.genreid }).exec(cb),
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (results.genre_books.length > 0) {
+        res.render("genre_delete", {
+          title: "Delete Genre",
+          genre: results.genre,
+          genre_books: results.genre_books,
+        });
+        return;
+      } else {
+        Genre.findByIdAndRemove(req.body.genreid, (err) => {
+          if (err) return next(err);
+          res.redirect("/catalog/genres");
+        });
+      }
+    }
+  );
 };
-
 // Display Genre update form on GET.
 exports.genre_update_get = function (req, res) {
   res.send("NOT IMPLEMENTED: Genre update GET");
